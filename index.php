5fc1d07ffebb4c8449a4f2284408edeca12c2dbe
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2022-08-16 12:15:55 -0300

    alteracao

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>Sistema de Login</title>
+    <title>Sistema Web 2.0</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -18,7 +18,7 @@
                     <div class="container-fluid">
                         <a class="navbar-brand" href="#">
                             <img src="img/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                            Bootstrap
+                            Sistema Web 2.0
                         </a>
                     </div>
                 </nav>
@@ -26,19 +26,18 @@
         </div>
         <div class="row">
             <div class="col">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Sistema Web 2.0</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav">
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+                                    <a class="nav-link active" aria-current="page" href="#">Cadastro</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Consultar</a>
+                                    <a class="nav-link" href="consulta.php">Consulta</a>
                                 </li>
                             </ul>
                         </div>
@@ -48,70 +47,83 @@
         </div>
         <div class="row">
             <div class="col">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nome:</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Endereço:</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Bairro:</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">CEP:</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Cidade:</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Estado:</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Selecionar...</option>
-                        <option value="1">AC</option>
-                        <option value="2">AL</option>
-                        <option value="3">AP</option>
-                        <option value="4">AM</option>
-                        <option value="5">BA</option>
-                        <option value="6">CE</option>
-                        <option value="7">ES</option>
-                        <option value="8">GO</option>
-                        <option value="9">MA</option>
-                        <option value="10">MT</option>
-                        <option value="11">MS</option>
-                        <option value="12">MG</option>
-                        <option value="13">PA</option>
-                        <option value="14">PB</option>
-                        <option value="15">PR</option>
-                        <option value="16">PE</option>
-                        <option value="17">PI</option>
-                        <option value="18">RJ</option>
-                        <option value="19">RN</option>
-                        <option value="20">RS</option>
-                        <option value="21">RO</option>
-                        <option value="22">RR</option>
-                        <option value="23">SC</option>
-                        <option value="24">SP</option>
-                        <option value="25">SE</option>
-                        <option value="26">TO</option>
-                        <option value="27">DF</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Login:</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">Senha</label>
-                    <input type="password" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <!-- As a heading -->
+                <nav class="navbar bg-light">
+                    <div class="container_fluid">
+                        <span class="navbar-brand">Cadastro de Usuário</span>
+                    </div>
+                </nav>
+            </div>
+            <div class="row">
+                <div class="col">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <form class="row g-3">
+                        <div class="col-md-6">
+                            <label for="inputEmail4" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="inputEmail4">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputPassword4" class="form-label">Senha</label>
+                            <input type="password" class="form-control" id="inputPassword4">
+                        </div>
+                        <div class="col-12">
+                            <label for="inputAddress" class="form-label">Endereço</label>
+                            <input type="text" class="form-control" id="inputAdress">
+                        </div>
+                        <div class="col-6">
+                            <label for="inputAdress2" class="form-label">Bairro</label>
+                            <input type="text" class="form-control" id="inputAdress2">
+                        </div>
+                        <div class="col-6">
+                            <label for="inputAddress2" class="form-label">Cep</label>
+                            <input type="text" class="form-control" id="inputAddress2">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputCity" class="form-label">Cidade</label>
+                            <input type="text" class="form-control" id="inputCity">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="inputState" class="form-label">Estado</label>
+                            <select id="inputState" class="form-select">
+                                <option selected>Escolha...</option>
+                                <option value="1">AC</option>
+                                <option value="2">AL</option>
+                                <option value="3">AP</option>
+                                <option value="4">AM</option>
+                                <option value="5">BA</option>
+                                <option value="6">CE</option>
+                                <option value="7">ES</option>
+                                <option value="8">GO</option>
+                                <option value="9">MA</option>
+                                <option value="10">MT</option>
+                                <option value="11">MS</option>
+                                <option value="12">MG</option>
+                                <option value="13">PA</option>
+                                <option value="14">PB</option>
+                                <option value="15">PR</option>
+                                <option value="16">PE</option>
+                                <option value="17">PI</option>
+                                <option value="18">RJ</option>
+                                <option value="19">RN</option>
+                                <option value="20">RS</option>
+                                <option value="21">RO</option>
+                                <option value="22">RR</option>
+                                <option value="23">SC</option>
+                                <option value="24">SP</option>
+                                <option value="25">SE</option>
+                                <option value="26">TO</option>
+                                <option value="27">DF</option>
+                            </select>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-dark">Cadastrar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
 </body>
 
 </html>
